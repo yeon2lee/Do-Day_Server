c695db4db0bea35db6ded8ce0a0f5693768c622b
yeon2lee <md04254@naver.com>
2023-07-27 00:20:26 +0900

    Initial commit

---
+++ src/main/java/com/project/doday/DodayApplication.java
@@ -0,0 +1,13 @@
+package com.project.doday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/doday/DodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.doday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
