1cd559d5be8b27aff3b1baff074c5b86abadd3c0
ziiyouth <ziy913@naver.com>
2023-07-31 03:06:27 +0900

    feat : 나의 신고 목록 보기 API 생성

--- src/main/java/com/project/doday/service/MemberService.java
+++ src/main/java/com/project/doday/service/MemberService.java
@@ -1,7 +1,10 @@
 package com.project.doday.service;
 
+import com.project.doday.domain.Report;
 import com.project.doday.domain.Solution;
+import com.project.doday.dto.MyReportRes;
 import com.project.doday.dto.SolutionListRes;
+import com.project.doday.repository.ReportRepository;
 import com.project.doday.repository.SolutionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @Transactional
@@ -16,6 +20,7 @@ import java.util.List;
 public class MemberService {
 
     private final SolutionRepository solutionRepository;
+    private final ReportRepository reportRepository;
 
     /**
      * 나의 해결 목록 보기
@@ -36,6 +41,26 @@ public class MemberService {
         }
 
         return mySolution;
+    }
+
+    /**
+     * 나의 신고 목록 보기
+     */
+    @Transactional(readOnly = true)
+    public ArrayList<MyReportRes> getMyReport(Long memberId){
+        List<Report> reports = reportRepository.findAll();
+        ArrayList<MyReportRes> myReport = new ArrayList<>();
+
+        for(Report report : reports) {
+            if(Objects.equals(report.getMember().getId(), memberId)){
+                MyReportRes myReportRes = new MyReportRes(
+                        report.getId(), report.getLocation(), report.getPhotoRaincatch(), report.getCreatedDate(),
+                        report.getState(), "");
+                myReport.add(myReportRes);
+            }
+        }
+        return myReport;
+
     }
 
 }
