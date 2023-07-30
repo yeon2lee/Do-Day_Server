c30d2bf686d54eb8801a924004a79e04f18fbdf8
yeon2lee <md04254@naver.com>
2023-07-30 17:06:28 +0900

    feat: 리워드 관련 MVC 추가

---
+++ src/main/java/com/project/doday/service/RewardService.java
@@ -0,0 +1,45 @@
+package com.project.doday.service;
+
+import com.project.doday.domain.Member;
+import com.project.doday.domain.Reward;
+import com.project.doday.domain.Solution;
+import com.project.doday.dto.RewardRes;
+import com.project.doday.repository.MemberRepository;
+import com.project.doday.repository.RewardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RewardService {
+
+    private final RewardRepository rewardRepository;
+    private final MemberRepository memberRepository;
+
+    /**
+     * 리워드 내역 확인하기
+     */
+    public List<Reward> getMyReward(Long memberId) {
+        List<Reward> rewards = rewardRepository.findAll();
+        List<Reward> myReward = new ArrayList<>();
+
+        for(Reward reward : rewards){
+            if(reward.getMember().getId() == memberId)
+                myReward.add(reward);
+        }
+        return myReward;
+    }
+
+    /**
+     * 리워드 전환하기
+     */
+    public Long convertReward(Long memberId, Long amount) {
+        Member member = memberRepository.findById(memberId).get();
+        member.covertReward(amount);
+        return member.getNowReward();
+    }
+}
