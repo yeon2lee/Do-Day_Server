d9b308d2f2707ef4c45a210fac58876dc2f48098
yeon2lee <md04254@naver.com>
2023-08-04 20:05:47 +0900

    fix: 반려 사유 불러오기

--- src/main/java/com/project/doday/repository/SolutionRejectRepository.java
+++ src/main/java/com/project/doday/repository/SolutionRejectRepository.java
@@ -3,5 +3,8 @@ package com.project.doday.repository;
 import com.project.doday.domain.SolutionReject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SolutionRejectRepository extends JpaRepository<SolutionReject, Long> {
+    Optional<SolutionReject> findBySolutionId(Long solutionId);
 }
