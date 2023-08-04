86ecefecda025ddfa14e4c94b68d066ce1813c72
ziiyouth <ziy913@naver.com>
2023-08-04 13:50:50 +0900

    fix : solution 승인/반려 테이블 수정

--- src/main/java/com/project/doday/domain/SolutionReject.java
+++ src/main/java/com/project/doday/domain/SolutionReject.java
@@ -1,10 +1,8 @@
 package com.project.doday.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
@@ -12,11 +10,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SolutionReject {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    private Long solutionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "admin_id")
+    @JsonIgnore
     private Admin admin;
 
     private String content;
+
+    @Builder
+    public SolutionReject(Long reportId, Admin admin, String content) {
+        this.solutionId = reportId;
+        this.admin = admin;
+        this.content = content;
+    }
 }
